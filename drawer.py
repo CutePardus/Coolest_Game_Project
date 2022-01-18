6facf83f4ab7c2d80d1aaf3b0d7ad461b37b95d7
CutePardus <nikacat2529@gmail.com>
2022-01-18 17:45:08 +0300

    фикс некоторых багов, добавленна полоска хп

--- drawer.py
+++ drawer.py
@@ -1,4 +1,6 @@
 import pygame
+from level_functions import load_image
+from database_helper import load_tool
 
 
 def classes(screen):
@@ -6,18 +8,30 @@ def classes(screen):
     font = pygame.font.Font(None, 50)
     text = font.render('Выберите класс героя', True, (255, 255, 255))
     screen.blit(text, (300, 125))
-    pygame.draw.rect(screen, 'green', (300, 200, 400, 100), 0)
-    pygame.draw.rect(screen, 'green', (300, 350, 400, 100), 0)
-    pygame.draw.rect(screen, 'green', (300, 500, 400, 100), 0)
-    pygame.draw.rect(screen, 'green', (300, 650, 400, 100), 0)
+    pygame.draw.rect(screen, 'white', (300, 200, 400, 100), 0)
+    pygame.draw.rect(screen, 'white', (300, 350, 400, 100), 0)
+    pygame.draw.rect(screen, 'white', (300, 500, 400, 100), 0)
+    pygame.draw.rect(screen, 'white', (300, 650, 400, 100), 0)
     text = font.render('Воин', True, (0, 0, 0))
     screen.blit(text, (300, 200))
+    w_img = load_image('warior.png', color_key=-1)
+    w_img = pygame.transform.scale(w_img, (75, 100))
+    screen.blit(w_img, (625, 200))
     text = font.render('Охотница', True, (0, 0, 0))
     screen.blit(text, (300, 350))
+    h_img = load_image('hunter.png', color_key=-1)
+    h_img = pygame.transform.scale(h_img, (75, 100))
+    screen.blit(h_img, (625, 350))
     text = font.render('Вор', True, (0, 0, 0))
     screen.blit(text, (300, 500))
+    t_img = load_image('thief.png', color_key=-1)
+    t_img = pygame.transform.scale(t_img, (75, 100))
+    screen.blit(t_img, (625, 500))
     text = font.render('Дриадна', True, (0, 0, 0))
     screen.blit(text, (300, 650))
+    d_img = load_image('driade.png', color_key=-1)
+    d_img = pygame.transform.scale(d_img, (75, 100))
+    screen.blit(d_img, (625, 650))
 
 
 def saves(screen):
@@ -25,10 +39,10 @@ def saves(screen):
     font = pygame.font.Font(None, 50)
     text = font.render('Выберите номер сохранения', True, (255, 255, 255))
     screen.blit(text, (250, 125))
-    pygame.draw.rect(screen, 'green', (300, 200, 400, 100), 0)
-    pygame.draw.rect(screen, 'green', (300, 350, 400, 100), 0)
-    pygame.draw.rect(screen, 'green', (300, 500, 400, 100), 0)
-    pygame.draw.rect(screen, 'green', (300, 650, 400, 100), 0)
+    pygame.draw.rect(screen, 'white', (300, 200, 400, 100), 0)
+    pygame.draw.rect(screen, 'white', (300, 350, 400, 100), 0)
+    pygame.draw.rect(screen, 'white', (300, 500, 400, 100), 0)
+    pygame.draw.rect(screen, 'white', (300, 650, 400, 100), 0)
     text = font.render('1', True, (0, 0, 0))
     screen.blit(text, (300, 200))
     text = font.render('2', True, (0, 0, 0))
--- player.py
+++ player.py
@@ -75,16 +75,29 @@ class Menu:
         if 300 <= x <= 700:
             if 200 <= y <= 300:
                 self.hero_class = 'Воин'
+                print(self.hero_class)
+                hero = self.hero_class
+                print(hero)
+                game_started = True
             elif 350 <= y <= 450:
                 self.hero_class = 'Охотница'
+                print(self.hero_class)
+                hero = self.hero_class
+                print(hero)
+                game_started = True
             elif 500 <= y <= 600:
                 self.hero_class = 'Вор'
+                print(self.hero_class)
+                hero = self.hero_class
+                print(hero)
+                game_started = True
             elif 650 <= y <= 750:
                 self.hero_class = 'Дриадна'
-        print(self.hero_class)
-        hero = self.hero_class
-        print(hero)
-        game_started = True
+                print(self.hero_class)
+                hero = self.hero_class
+                print(hero)
+                game_started = True
+
 
     def get_click(self, mouse_pos):
         if not self.save_chosen:
@@ -131,62 +144,77 @@ class Board:
         self.cell_size = cell_size
 
     def render(self, screen):
-        for row in range(len(self.board)):
-            for column in range(len(self.board[row])):
-                x, y = column * self.cell_size + self.left, row * self.cell_size + self.top
-                floor_img = load_image('floor.png')
-                floor_img = pygame.transform.scale(floor_img, (50, 50))
-                screen.blit(floor_img, (x, y))
-                if self.board[column][row] == '1':
-                    chest_img = load_image('Chest.png')
-                    chest_img = pygame.transform.scale(chest_img, (50, 50))
-                    screen.blit(chest_img, (100 + column * self.cell_size, 100 + row * self.cell_size))
-                elif self.board[column][row] != '0' and self.board[column][row] != 'exit':
-                    img = load_image(str(load_tool(self.board[column][row])['pic']), color_key=-1)
-                    img = pygame.transform.scale(img, (50, 50))
-                    screen.blit(img, (100 + column * self.cell_size, 100 + row * self.cell_size))
-                elif self.board[column][row] == 'exit':
-                    exit_img = load_image('exit.png')
-                    exit_img = pygame.transform.scale(exit_img, (50, 50))
-                    screen.blit(exit_img, (100 + column * self.cell_size, 100 + row * self.cell_size))
-                elif column == 0 and row == 0:
-                    entrance_img = load_image('entrance.png')
-                    entrance_img = pygame.transform.scale(entrance_img, (50, 50))
-                    screen.blit(entrance_img, (100 + column * self.cell_size, 100 + row * self.cell_size))
-        s = set()
-        for elem in self.current_loot:
-            s.add(elem)
-        self.loot2 = []
-        self.loot2.extend(s)
-        pygame.draw.rect(screen, (54, 11, 204), (900, 250, 75, 300))
-        for i in range(len(self.loot2)):
-            pygame.draw.rect(screen, (255, 255, 255), (901, 251 + i * 75, 74, 74))
-            img = load_image(str(load_tool(self.loot2[i])['pic']), color_key=-1)
-            img = pygame.transform.scale(img, (75, 75))
-            screen.blit(img, (900, 250 + i * 75))
-            text = str(self.current_loot.count(self.loot2[i]))
-            font = pygame.font.Font(None, 50)
-            text = font.render(text, True, (0, 0, 0))
-            screen.blit(text, (960, 310 + i * 75))
-        pygame.draw.rect(screen, 'red', (900, 10, 75, 20))
-        font = pygame.font.Font(None, 20)
-        text = font.render('Выход', True, (255, 254, 254))
-        screen.blit(text, (910, 10))
-        if len(self.current_loot) == 1:
-            self.current_loot = []
-            self.current_loot.append(load_hero(hero)['start_loot'])
-        hero_img = load_image(load_hero(hero)['pic'], color_key=-1)
-        hero_img = pygame.transform.scale(hero_img, (50, 50))
-        screen.blit(hero_img, (100 + self.current_pos[0] * self.cell_size, 100 + self.current_pos[1] * self.cell_size))
+        if game_started:
+            for row in range(len(self.board)):
+                for column in range(len(self.board[row])):
+                    x, y = column * self.cell_size + self.left, row * self.cell_size + self.top
+                    floor_img = load_image('floor.png')
+                    floor_img = pygame.transform.scale(floor_img, (50, 50))
+                    screen.blit(floor_img, (x, y))
+                    if self.board[column][row] == '1':
+                        chest_img = load_image('Chest.png')
+                        chest_img = pygame.transform.scale(chest_img, (50, 50))
+                        screen.blit(chest_img, (100 + column * self.cell_size, 100 + row * self.cell_size))
+                    elif self.board[column][row] != '0' and self.board[column][row] != 'exit':
+                        img = load_image(str(load_tool(self.board[column][row])['pic']), color_key=-1)
+                        img = pygame.transform.scale(img, (50, 50))
+                        screen.blit(img, (100 + column * self.cell_size, 100 + row * self.cell_size))
+                    elif self.board[column][row] == 'exit':
+                        exit_img = load_image('exit.png')
+                        exit_img = pygame.transform.scale(exit_img, (50, 50))
+                        screen.blit(exit_img, (100 + column * self.cell_size, 100 + row * self.cell_size))
+                    elif column == 0 and row == 0:
+                        entrance_img = load_image('entrance.png')
+                        entrance_img = pygame.transform.scale(entrance_img, (50, 50))
+                        screen.blit(entrance_img, (100 + column * self.cell_size, 100 + row * self.cell_size))
+            s = set()
+            for elem in self.current_loot:
+                s.add(elem)
+            self.loot2 = []
+            self.loot2.extend(s)
+            pygame.draw.rect(screen, (54, 11, 204), (900, 250, 75, 300))
+            for i in range(len(self.loot2)):
+                pygame.draw.rect(screen, (255, 255, 255), (901, 251 + i * 75, 74, 74))
+                img = load_image(str(load_tool(self.loot2[i])['pic']), color_key=-1)
+                img = pygame.transform.scale(img, (75, 75))
+                screen.blit(img, (900, 250 + i * 75))
+                text = str(self.current_loot.count(self.loot2[i]))
+                font = pygame.font.Font(None, 50)
+                text = font.render(text, True, (0, 0, 0))
+                screen.blit(text, (960, 300 + i * 75))
+            pygame.draw.rect(screen, 'red', (900, 10, 75, 20))
+            font = pygame.font.Font(None, 20)
+            text = font.render('Выход', True, (255, 254, 254))
+            screen.blit(text, (910, 10))
+            if len(self.current_loot) == 1:
+                self.current_loot = []
+                self.current_loot.append(load_hero(hero)['start_loot'])
+            hero_img = load_image(load_hero(hero)['pic'], color_key=-1)
+            hero_img = pygame.transform.scale(hero_img, (50, 50))
+            screen.blit(hero_img, (100 + self.current_pos[0] * self.cell_size, 100 + self.current_pos[1] * self.cell_size))
+            pygame.draw.rect(screen, 'white', (25, 25, load_hero(hero)['health'] * 5, 20))
+            pygame.draw.rect(screen, 'red', (25, 25, self.health * 5, 20))
+            font = pygame.font.Font(None, 20)
+            text = font.render(str(self.health), True, (255, 254, 254))
+            screen.blit(text, (25, 10))
 
     def get_cell(self, mouse_pos):
         if 900 <= mouse_pos[0] <= 975 and 10 <= mouse_pos[1] <= 30:
             print('ok')
             # save_game(self.save_id, )
             return tuple([16, 16])
-        elif 900 <= mouse_pos[0] <= 975 and 250 <= mouse_pos[1] <= 325:
-            self.active_item(len(self.loot2) - 1)
-            return 0, 0
+        elif 900 <= mouse_pos[0] <= 975 and 250 < mouse_pos[1] <= 325:
+            self.active_item(0)
+            return 16, 16
+        elif 900 <= mouse_pos[0] <= 975 and 325 < mouse_pos[1] <= 400 and len(self.loot2) >= 1:
+            self.active_item(1)
+            return 16, 16
+        elif 900 <= mouse_pos[0] <= 975 and 400 < mouse_pos[1] <= 475 and len(self.loot2) >= 2:
+            self.active_item(2)
+            return 16, 16
+        elif 900 <= mouse_pos[0] <= 975 and 550 < mouse_pos[1] <= 625 and len(self.loot2) >= 3:
+            self.active_item(3)
+            return 16, 16
         else:
             x, y = (mouse_pos[0] - self.top) // self.cell_size, (mouse_pos[1] - self.left) // self.cell_size
             if 0 <= x <= self.width and 0 <= y <= self.height:
@@ -204,7 +232,7 @@ class Board:
                     self.target_choosing = False
                     self.current_loot.remove(self.item)
             else:
-                print('nigger')
+                print(self.item)
                 if load_tool(self.item)['rarity'] != 1:
                     self.target_choosing = False
                     self.current_loot.remove(self.item)
@@ -221,10 +249,20 @@ class Board:
             self.item = self.loot2[item_num]
             print(item_num)
         if 'heal' in load_tool(self.loot2[item_num])['abilities'].split(', '):
-            if self.health <= int(load_hero(hero)['health']) + load_tool(self.loot2[item_num])['ability_num']:
+            self.item = self.loot2[item_num]
+            if self.health + load_tool(self.loot2[item_num])['ability_num'] <= int(load_hero(hero)['health']):
                 self.health += load_tool(self.loot2[item_num])['ability_num']
+                if load_tool(self.item)['rarity'] != 1:
+                    self.current_loot.remove(self.item)
+            elif self.health != int(load_hero(hero)['health']):
+                self.health = int(load_hero(hero)['health'])
+                if load_tool(self.item)['rarity'] != 1:
+                    self.current_loot.remove(self.item)
+            else:
+                print('ваше здоровье и так максимальное')
 
     def new_level(self):
+        global current_pos
         self.board = [['0' for i in range(self.width)] for _ in range(self.height)]
         for i in range(15):
             m = random.randint(0, self.height - 1)
@@ -232,6 +270,8 @@ class Board:
             self.board[m][n] = '1'
         self.board[self.height - 1][self.width - 1] = 'exit'
         self.current_pos = [0, 0, current_pos[2] + 1]
+        current_pos = [0, 0, current_pos[2] + 1]
+        print(self.board)
 
     def exit(self):
         global running
@@ -246,19 +286,24 @@ class Board:
         if unicod == 's':
             if self.current_pos[1] != 14:
                 self.current_pos[1] += 1
+                current_pos[1] += 1
         elif unicod == 'w':
             if self.current_pos[1] != 0:
                 self.current_pos[1] -= 1
+                current_pos[1] -= 1
         elif unicod == 'a':
             if self.current_pos[0] != 0:
                 self.current_pos[0] -= 1
+                current_pos[0] -= 1
         elif unicod == 'd':
             if self.current_pos[0] != 14:
                 self.current_pos[0] += 1
+                current_pos[0] += 1
         elif unicod == ' ':
             if self.board[current_pos[0]][current_pos[1]] == '1':
                 self.board[current_pos[0]][current_pos[1]] = random.choice(all_loot)
             elif self.board[current_pos[0]][current_pos[1]] == 'exit':
+                print(current_pos)
                 self.new_level()
             elif self.board[current_pos[0]][current_pos[1]] != '0':
                 self.current_loot.append(self.board[current_pos[0]][current_pos[1]])
